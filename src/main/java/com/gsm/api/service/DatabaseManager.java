8afb3688317f7038d182fbca1c8dbaf25147b177
Andrei Smadu <smaduandrei451@gmail.com>
2026-05-31 17:52:06 +0300

    short mod

--- src/main/java/com/gsm/api/service/DatabaseManager.java
+++ src/main/java/com/gsm/api/service/DatabaseManager.java
@@ -3,13 +3,26 @@ package com.gsm.api.service;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE"; // sau /XEPDB1
-    private static final String USER = "USERPAOJ";
-    private static final String PASSWORD = "a1234";
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(DatabaseManager.class.getClassLoader().getResourceAsStream("database.properties"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private DatabaseManager() {}
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(
+                properties.getProperty("db.url"),
+                properties.getProperty("db.user"),
+                properties.getProperty("db.password")
+        );
     }
 }
